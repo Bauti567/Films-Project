70c65b89d8cbee6ebd1147af8f6d06874c1ab92f
Juan Bautista <jbautistapulido@gmail.com>
2024-10-14 15:56:13 -0500

    Database is connected

---
+++ backend/src/db.js
@@ -0,0 +1,13 @@
+// conexion con base de datos
+import mongoose, { mongo } from "mongoose";
+
+export const ConnectDB = async()=>{
+    try{
+        await mongoose.connect('mongodb+srv://JuanBautista:JuanBautista1972@atlascluster.lazttss.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
+        console.log('Db is connected')
+    
+    }catch(error){
+        console.log(error)
+    }
+
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import { ConnectDB } from './db.js'
 
 const app = express()
-const PORT = 3000
 
+
+ConnectDB()
+const PORT = 3000
 app.listen(PORT,()=>{
     console.log('App is running on port: ',PORT)
 })
